16bdce4e9c9f49402c1a4c374ac02106802cc041
andikarafon <andika.rafon.senohaji@gmail.com>
2025-12-15 13:18:54 +0700

    Initial Upload

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_kasir/core/di/dependency.dart';
+
+void main() {
+  initDependency();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(colorSchemeSeed: Colors.red),
+      home: Scaffold(body: Center(child: Text('Ravon & Wulan'))),
+    );
+  }
+}
